6fa223c76fdc94bcbf05e947828e8659bb70584f
Scott Stirling <scottstirling@gmail.com>
2026-06-20 18:56:55 -0400

    Add .gitignore for build stuff and update build-dist.sh to prep for release

--- build-dist.sh
+++ build-dist.sh
@@ -2,17 +2,23 @@
 
 NAME=AnnotateImageExt
 BUILD_DIR=./build
+DIST_DIR=./dist
 DATE=`date +%F` # yyyy-mm-dd
-TGZ_FILENAME="${DATE}-AnnotateImageExt.tar.gz"
+TGZ_FILENAME="${DATE}-${NAME}.tar.gz"
 
 mkdir -vp $BUILD_DIR/doc/scripts/$NAME
 mkdir -vp $BUILD_DIR/src/scripts/$NAME
 
 #TODO: update for AnnotateImageExt
-cp -v pi2llm-main.js etc/pi2llm-main.xsgn AGENTS.md README.md LICENSE $BUILD_DIR/src/scripts/$NAME/
-cp -rv lib $BUILD_DIR/src/scripts/$NAME/
-cp -rv doc/scripts/$NAME/*.html $BUILD_DIR/doc/scripts/$NAME/
+cp -vR src/scripts/${NAME}/ etc/*.xsgn README.md LICENSE ${BUILD_DIR}/src/scripts/${NAME}/
+cp -rv doc/scripts/${NAME}/*.html ${BUILD_DIR}/doc/scripts/${NAME}/
 
-tar -C $BUILD_DIR -czvf $TGZ_FILENAME doc/ src/
+tar -C ${BUILD_DIR} -czvf ${TGZ_FILENAME} doc/ src/
 
-sha1sum $TGZ_FILENAME
+echo "sha1sum ${TGZ_FILENAME}: `sha1sum ${TGZ_FILENAME}`"
+
+mkdir -vp ${DIST_DIR}
+mv ${TGZ_FILENAME} ${DIST_DIR}/
+cp -v etc/updates.xri ${DIST_DIR}/
+
+echo "Build and distribution file creation done."
